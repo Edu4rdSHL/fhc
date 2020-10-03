e1ecd57557e4d554319e248cca856bbf4a38d633
Eduard Tolosa <edu4rdshl@protonmail.com>
2020-10-03 16:12:39 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,39 @@
+# FHC
+FHC stands for Fast HTTP Checker, it's written in Rust. Works on Linux, Windows, macOS, Android, Aarch64, ARM and possibly in your oven.
+
+# Goal
+Offer the community an efficient HTTP checker tool.
+
+# Methodology
+FHC try to resolve https first, if this fails then fallback to http, in that way you don't miss any active HTTP host.
+
+# Performance & speed
+FHC is **very** resource friendly, you can use up to 1000 threads in an single core machine and this will work without any problem, the bottleneck for this tool is your network speed. By default, FHC is able to perform HTTP check for ~76 hosts per second in good network conditions (tested in an Google Cloud machine). Depending on how much host have only http (not https) and/or are alive the number of host resolved in average can be higher as the tool doesn't have to perform an double check. In our demo we used an real-world scenario performing resolution for all the `facebook.com` subdomains.
+
+## Demo
+[![asciicast](https://asciinema.org/a/363351.svg)](https://asciinema.org/a/363351)
+
+# Installation
+
+## Using precompiled binaries.
+
+Download the asset from the [releases page](https://github.com/Edu4rdSHL/fhc/releases/latest) according to your platform.
+
+## Using the source code.
+
+1. You need to have the lastest stable [Rust](https://www.rust-lang.org/) version insalled in your system.
+2. Clone the repo or download the source code, then run `cargo build --release`.
+3. Execute the tool from `./target/release/fhc` or add it to your system PATH to use from anywhere.
+
+Optionally you can just use `cargo install fhc`
+
+# Usage
+* Show all HTTP urls depite their response codes:
+```
+cat hosts.txt | fhc
+```
+* If you want to see only the HTTP host with 200-299 codes:
+```
+cat hosts.txt | fhc -2
+```
+You can tune the `--timeout`, `-t/--threads`, `-u/--user-agent` and other options according to your needs. See `fhc --help`
---
+++ src/main.rs
@@ -0,0 +1,127 @@
+use {
+    clap::{value_t, App, Arg},
+    futures::stream::StreamExt,
+    reqwest,
+    tokio::{
+        self,
+        io::{self, AsyncReadExt},
+    },
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Eval args
+    let matches = App::new("Fhc")
+        .version("0.1.0")
+        .author("Eduard Tolosa <edu4rdshl@protonmail.com>")
+        .about("Fast HTTP Checker.")
+        .arg(
+            Arg::with_name("threads")
+                .short("t")
+                .long("threads")
+                .takes_value(true)
+                .help("Number of threads. Default: 500"),
+        )
+        .arg(
+            Arg::with_name("timeout")
+                .long("timeout")
+                .takes_value(true)
+                .help("Timeout in seconds. Default: 1"),
+        )
+        .arg(
+            Arg::with_name("user-agent")
+                .short("u")
+                .long("user-agent")
+                .takes_value(true)
+                .help("User agent string."),
+        )
+        .arg(
+            Arg::with_name("1xx")
+                .short("1")
+                .long("1xx")
+                .takes_value(false)
+                .help("Show URLs with 100-199 response codes only."),
+        )
+        .arg(
+            Arg::with_name("2xx")
+                .short("2")
+                .long("2xx")
+                .takes_value(false)
+                .help("Show URLs with 200-299 response codes only."),
+        )
+        .arg(
+            Arg::with_name("3xx")
+                .short("3")
+                .long("3xx")
+                .takes_value(false)
+                .help("Show URLs with 300-399 response codes only."),
+        )
+        .arg(
+            Arg::with_name("4xx")
+                .short("4")
+                .long("4xx")
+                .takes_value(false)
+                .help("Show URLs with 400-499 response codes only."),
+        )
+        .arg(
+            Arg::with_name("5xx")
+                .short("5")
+                .long("5xx")
+                .takes_value(false)
+                .help("Show URLs with 500-599 response codes only."),
+        )
+        .get_matches();
+
+    // Assign values or use defaults
+    let conditional_response_code = if matches.is_present("1xx") {
+        100
+    } else if matches.is_present("2xx") {
+        200
+    } else if matches.is_present("3xx") {
+        300
+    } else if matches.is_present("4xx") {
+        400
+    } else if matches.is_present("5xx") {
+        500
+    } else {
+        0
+    };
+    let threads = value_t!(matches.value_of("threads"), usize).unwrap_or_else(|_| 100);
+    let timeout = value_t!(matches.value_of("timeout"), u64).unwrap_or_else(|_| 2);
+    let user_agent = &value_t!(matches.value_of("user-agent"), String).unwrap_or_else(|_| "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36".to_string());
+
+    // Read stdin
+    let mut buffer = String::new();
+    let mut stdin = io::stdin();
+    stdin.read_to_string(&mut buffer).await?;
+    let hosts: Vec<String> = buffer.lines().map(str::to_owned).collect();
+
+    futures::stream::iter(hosts.into_iter().map(|host| async move {
+        let mut response_code = 0;
+        let mut protocol = String::new();
+        let client = reqwest::Client::builder()
+            .timeout(std::time::Duration::from_secs(timeout))
+            .user_agent(user_agent)
+            .build()
+            .unwrap();
+        if let Ok(resp) = client.get(&format!("https://{}", host)).send().await {
+            protocol = "https://".to_string();
+            response_code = resp.status().as_u16();
+        } else if let Ok(resp) = client.get(&format!("http://{}", host)).send().await {
+            protocol = "http://".to_string();
+            response_code = resp.status().as_u16()
+        }
+        if !protocol.is_empty() && conditional_response_code == 0 {
+            println!("{}", protocol + &host)
+        } else if (!protocol.is_empty() && conditional_response_code != 0)
+            && (response_code >= conditional_response_code
+                && response_code <= conditional_response_code + 99)
+        {
+            println!("{}", protocol + &host)
+        }
+    }))
+    .buffer_unordered(threads)
+    .collect::<Vec<()>>()
+    .await;
+    Ok(())
+}
